f20a1fb024f6689f155bd4159abe29c7ee543669
demiladebamgbose <demiladebamgbose94@gmail.com>
2016-05-27 10:35:04 +0100

    fix indentation

--- js/login.js
+++ js/login.js
@@ -1,38 +1,37 @@
 var events = [];
-
-
 function start(){
 	registerClickEvent('signIn', signin);
-	getId('msg').style.color='red';
+ 	getId('msg').style.color='red';
 }
-
+	
 function signin(){
-	var found;
-	if( (isValidData('email')) && (isValidData('password')) ){
-		if(isEmail('email')){
-				console.log(getId('email').value.split('@')[0] );
-				var myUserRef = new Firebase('https://icalendra.firebaseio.com/'+ getId('email').value.split('@')[0]);
-				myUserRef.on("value", function(snapshot) {
-				var message = snapshot.val();
-				
-				// if(message=== null){
-				// 	getId('msg').innerHTML='Invalid Username or password';
-				// 	return;
-				// }
-				if( (message.email == getId('email').value) && (message.password === getId('password').value)){
+  var found;
+  if( (isValidData('email')) && (isValidData('password')) ){
+    if(isEmail('email')){
+		  console.log(getId('email').value.split('@')[0] );
+		  var myUserRef = new Firebase('https://icalendra.firebaseio.com/'+ getId('email').value.split('@')[0]);
+		  myUserRef.on("value", function(snapshot) {
+			  var message = snapshot.val();	
+
+			  if(message=== null){
+					getId('msg').innerHTML='Invalid Username or password';
+				  return;
+			  }
+
+			  if( (message.email == getId('email').value) && (message.password === getId('password').value)){
 					localStorage.setItem("database",getId('email').value.split('@')[0]);
 					localStorage.setItem('currentUser', message.fullName);
 					console.log(localStorage.getItem('database'));
-				   	console.log(localStorage.getItem('currentUser'));
-				   	linkTo('event.html');
+					console.log(localStorage.getItem('currentUser'));
+					linkTo('event.html');
+			  }
+			  else{
+					getId('msg').innerHTML='Invalid Username or password';
 				}
-				else{
-		  			getId('msg').innerHTML='Invalid Username or password';
-		    	}
 			});
 		}
 		else{
-	    	getId('msg').innerHTML='Invalid Email Address';
+	  	getId('msg').innerHTML='Invalid Email Address';
 		}
 	}
 	else{
